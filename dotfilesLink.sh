28751d875082010b9113936671ab7a66f95b5bfa
localmin <localmin9201009@gmail.com>
2017-07-30 01:57:11 +0900

    avoid recursion links

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # vim
-ln -sf ~/dotfiles/vim/colors ~/.vim
-ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
+# Maybe you should wake up gvim once...
+mkdir -p ~/.vim
+mkdir -p ~/dotfiles/vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git \
-    ~/.vim/dein/repos/github.com/Shougo/dein.vim
+    ~/dotfiles/vim/dein/repos/github.com/Shougo/dein.vim
+ln -sf ~/dotfiles/vim/dein ~/.vim/dein
 ln -sf ~/dotfiles/vim/.dein.toml ~/.dein.toml
+ln -sf ~/dotfiles/vim/colors ~/.vim/colors
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 
 # sublimetext3
 ln -sf ~/dotfiles/sublimetext3/Packages ~/.config/sublime-text-3/Packages
