78200b6bda214fcb97f5d35299ec8c4f61699c1e
localmin <localmin9201009@gmail.com>
2018-03-04 01:09:49 +0900

    Fix links

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
+#gitconfig
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 # vim
 # Maybe you should wake up gvim once...
 mkdir -p ~/.vim
 mkdir -p ~/dotfiles/vim/dein/repos/github.com/Shougo/dein.vim
 git clone https://github.com/Shougo/dein.vim.git \
     ~/dotfiles/vim/dein/repos/github.com/Shougo/dein.vim
-ln -sf ~/dotfiles/vim/dein ~/.vim/dein
+ln -sf ~/dotfiles/vim/dein ~/.vim
 ln -sf ~/dotfiles/vim/.dein.toml ~/.dein.toml
-ln -sf ~/dotfiles/vim/colors ~/.vim/colors
+ln -sf ~/dotfiles/vim/colors ~/.vim
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gitconfig ~/.vimrc
 
 # etc
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
